4560030086c26121a227e8f23ba61051072dbdc2
jkadbear <jkadbear@users.noreply.github.com>
2023-05-15 19:20:15 +0800

    Fix GUI problem

--- docker_run.sh
+++ docker_run.sh
@@ -3,5 +3,5 @@
 DOCKER_XAUTH=/tmp/.docker.xauth
 touch /tmp/.docker.xauth
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $DOCKER_XAUTH nmerge -
-docker run -i -t --rm --privileged -e DISPLAY=$DISPLAY -e XAUTHORITY=$DOCKER_XAUTH -v /tmp/.docker.xauth:/tmp/.docker.xauth:ro -v /tmp/.X11-unix:/tmp/.X11-unix:ro -v /dev/bus/usb:/dev/bus/usb --entrypoint /bin/bash jkadbear/gr-lora:latest 
-# docker run -i -t --rm --privileged -e DISPLAY=$DISPLAY -e XAUTHORITY=$DOCKER_XAUTH -v /tmp/.docker.xauth:/tmp/.docker.xauth:ro -v /tmp/.X11-unix:/tmp/.X11-unix:ro -v /dev/bus/usb:/dev/bus/usb --mount src="$(pwd)",target=/src,type=bind --entrypoint /bin/bash jkadbear/gr-lora:latest 
+docker run -i -t --rm --privileged -e DISPLAY=$DISPLAY -e XAUTHORITY=$DOCKER_XAUTH -v /tmp/.docker.xauth:/tmp/.docker.xauth:ro -v /tmp/.X11-unix:/tmp/.X11-unix:ro --volume="$HOME/.Xauthority:/root/.Xauthority:rw" --net=host -v /dev/bus/usb:/dev/bus/usb --entrypoint /bin/bash jkadbear/gr-lora:latest 
+# docker run -i -t --rm --privileged -e DISPLAY=$DISPLAY -e XAUTHORITY=$DOCKER_XAUTH -v /tmp/.docker.xauth:/tmp/.docker.xauth:ro -v /tmp/.X11-unix:/tmp/.X11-unix:ro --volume="$HOME/.Xauthority:/root/.Xauthority:rw" --net=host -v /dev/bus/usb:/dev/bus/usb --mount src="$(pwd)",target=/src,type=bind --entrypoint /bin/bash jkadbear/gr-lora:latest 
